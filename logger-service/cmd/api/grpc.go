5a947523151fdda3b21868a4f78f05575a40e857
EdvardH7 <158154477+EdvardH7@users.noreply.github.com>
2024-03-26 12:11:18 +0100

    Add files via upload

---
+++ logger-service/cmd/api/grpc.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"logger/data"
+	"logger/logs"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type logServer struct {
+	logs.UnimplementedLogServiceServer
+	Models data.Models
+}
+
+func (l *logServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.LogResponse, error) {
+	input := req.GetLogentry()
+
+	logEntry := data.LogEntry{
+		Name: input.Name,
+		Data: input.Data,
+	}
+
+	err := l.Models.LogEntry.Insert(logEntry)
+	if err != nil {
+		res := &logs.LogResponse{Result: "failed"}
+		return res, err
+	}
+
+	res := &logs.LogResponse{Result: "logged"}
+	return res, err
+}
+
+func (app *Config) gRPCListen() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", gRpcPort))
+	if err != nil {
+		log.Fatalf("Failed to listen for gRPC: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	logs.RegisterLogServiceServer(s, &logServer{Models: app.Models})
+
+	log.Printf("gRPC Server started on port %s", gRpcPort)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to listen for gRPC: %v", err)
+	}
+}
